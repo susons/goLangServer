59bb0fb44d04a01c69eeab8f760e81e01c33739c
Kristaps Buls <kristaps.buls@Kristapss-MacBook-Air.local>
2025-01-21 01:45:44 +0200

    [add user auth]

---
+++ utils/jwt.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const ValidationKey = "move-to-env-key-for-test-purposes"
+
+func GenerateToken(email string, userId int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
+		"exp":    time.Now().Add(time.Hour * 2).Unix(),
+	})
+
+	return token.SignedString([]byte(ValidationKey))
+}
+
+func verifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return ValidationKey, nil
+	})
+
+	if err != nil {
+		return errors.New("Could not parse token")
+	}
+
+	valid := parsedToken.Valid
+
+	if !valid {
+		return errors.New("invalid token")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return errors.New("invalid token claims")
+	}
+
+	return nil
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+}
